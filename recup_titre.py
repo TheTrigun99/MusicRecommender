89c8395c37ed62ecf8cb3111f534838ce46171fe
TheTrigun99 <damien.aupretre@gmail.com>
2025-11-03 21:51:23 +0100

    suite

--- recup_titre.py
+++ recup_titre.py
@@ -3,14 +3,14 @@ import time
 def recupliked(sp_client):
     """Pagine sur toute la librairie 'Liked Songs' (50 par page)."""
     titres = []
-    limit = 50 #spot limite à 50 sons
+    limit = 50 #spot limite à 50 sons par requête !
     count = 0 
     while True:
         try:
             page = sp_client.current_user_saved_tracks(limit=limit, offset=count)
         except HTTPError as e:
-            if e.response is not None and e.response.status_code == 429:
-                retry_after = int(e.response.headers.get("Retry-After", "2"))
+            if e.response is not None and e.response.status_code == 429: #erreur 429 consiste à l'érreur quand on fait trop de reqîete à la fois (rate)
+                retry_after = int(e.response.headers.get("Retry-After", "2")) #il faut pateinter le temps demandé
                 time.sleep(retry_after + 1)
                 continue
             raise
