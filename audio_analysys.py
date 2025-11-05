543ca0f00c5cb5403c7dc21f04e929b275ab166a
TheTrigun99 <damien.aupretre@gmail.com>
2025-11-05 18:31:40 +0100

    étape 1 analyse

--- audio_analysys.py
+++ audio_analysys.py
@@ -3,13 +3,12 @@ import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 
-# ---- Paramètres (à ajuster selon le niveau de détail voulu)
 audio_path = "BlindingLights.mp3"
-sr_target   = None        # None = garder le sr d'origine (sinon, ex: 22050)
+sr_target   = None        
 n_fft       = 2048        # taille de fenêtre FFT
 hop_length  = 512         # pas entre fenêtres (samples)
 win         = "hann"      # fenêtre
-# ----
+
 
 # Charger l'audio (mono par défaut; mettre mono=False pour garder la stéréo)
 y, sr = librosa.load(audio_path, sr=sr_target, mono=True)
@@ -20,9 +19,22 @@ S_db = librosa.amplitude_to_db(S, ref=np.max)
 
 # Affichage
 plt.figure(figsize=(12, 5))
-librosa.display.specshow(S_db, sr=sr, hop_length=hop_length,
-                         x_axis='time', y_axis='log')  # y_axis='log' = échelle fréquentielle log
+librosa.display.specshow(S_db, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log')  # y_axis='log' = échelle fréquentielle log
 plt.colorbar(format="%+2.0f dB", label="Amplitude (dB)")
 plt.title("Spectrogramme (STFT) - échelle log")
 plt.tight_layout()
 plt.show()
+def audio_features(m):
+    "Prend un audio_path et renvoit un dictionnaire de features liés à la musique,"
+    "on essaye "
+    d={}
+    sr_target   = None        
+    n_fft       = 2048        # taille de fenêtre FFT
+    hop_length  = 512         # pas entre fenêtres (samples)
+    win         = "hann"   
+    y, sr = librosa.load(m, sr=sr_target, mono=True) #load la musique
+    Spectro = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length, window=win))
+    d['tempo']=librosa.feature.tempo(sr=sr,S=Spectro)
+    return d
+
+    
--- recuperinfos.py
+++ recuperinfos.py
@@ -20,3 +20,22 @@ def recupliked(sp_client):
         titres.extend(batch)
         count += limit #on récupère les sons 0-49 puis 50-99 etc
     return titres
+
+def audiofeatures(sp,titres):
+    idss=[titre['track']['id'] for titre in titres]
+    audio_features=[]
+    for i in range(0,len(idss),100): # 100 sons par reqûete (limite)
+        while True:
+            try: 
+                page = sp.audio_features(idss[i:i+100])
+            except HTTPError as e:
+                if e.response is not None and e.response.status_code == 429: #erreur 429 consiste à l'érreur quand on fait trop de reqîete à la fois (rate)
+                    retry_after = int(e.response.headers.get("Retry-After", "2")) #il faut pateinter le temps demandé
+                    time.sleep(retry_after + 1)
+                    continue
+                raise
+            if not batch:
+                break
+            audio_features=audio_features.extend(batch)
+
+    return audio_features
