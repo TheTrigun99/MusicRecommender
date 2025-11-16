2e358b7b825824a22a1a0b75b44a1f9882c76029
TheTrigun99 <damien.aupretre@gmail.com>
2025-11-16 18:39:16 +0100

    build, ça traite les centres et les fenêtres

--- item2vecscratch/build.py
+++ item2vecscratch/build.py
@@ -1,6 +1,8 @@
 from collections import Counter
 import numpy as np
-def build_vocab(playlists, min_count=1):
+from loading import load500
+# TODO: description fonction, regarder dataset en détail
+def build_vocab(playlists, min_count=3):
     counter = Counter()
 
     for pl in playlists:
@@ -10,14 +12,52 @@ def build_vocab(playlists, min_count=1):
 
     id2track = filtre
     counts = np.array([counter[item] for item in id2track], dtype=np.float32)
-
-    return track2id, id2track, counts
+    p_filtre= [
+        [track for track in pl if track in filtre]
+        for pl in playlists
+    ]
+    return track2id, id2track, counts, p_filtre
 
 def build_noise(count,alpha=0.75):
     noise= count**alpha
     noise_dist=noise/noise.sum()
     return noise_dist
 
-def gen_couples(p,t2id,window):
 
+def gen_couples1(p,t2id,wdow):
+    pairs=[]
+    for pl in p:
+        for i in range(len(pl)):
+            for p1 in range(1,min(wdow,i)+1):
+
+                pairs.append((t2id[pl[i]],t2id[pl[(i-p1)]]))
+
+            for p2 in range(1,min(wdow,len(pl)-i-1) +1):
+
+                pairs.append((t2id[pl[i]],t2id[pl[i+p2]]))
     return pairs
+
+def gen_couples(p,t2id,wdow):
+    pairs=[]
+    for pl in p:
+        for i in range(len(pl)):
+            centre=t2id[pl[i]]
+
+            #fenetre <-
+            for k in range(1,wdow+1):
+                p1=i-k
+                if i-k<0:
+                    break
+                pairs.append((centre,t2id[pl[p1]]))
+            
+            #fenetre ->
+            for l in range(1,wdow+1):
+                p2=i+l
+                if p2>=len(pl):
+                    break
+                pairs.append((centre,t2id[pl[p2]]))
+    return pairs
+
+p=load500('C:\\Users\\damie\Documents\\MusicRecommender\\spotify_dataset.csv')
+track2id, id2track, counts,pl=build_vocab(p,3)
+print(gen_couples(pl,track2id,2))
--- item2vecscratch/loading.py
+++ item2vecscratch/loading.py
@@ -16,4 +16,19 @@ def load_playlists(path):
     )
     return playlists
 
-        
+def load500(path):
+    df = pd.read_csv(
+    path,
+    on_bad_lines="skip",
+    nrows=50000,
+    engine="c")
+    df.columns = df.columns.str.strip().str.strip('"')
+
+    # groupby par playlistname → liste de tracks
+    playlists = (
+        df.groupby("playlistname")["trackname"]
+          .apply(list)
+          .tolist()
+    )
+    return playlists
+
